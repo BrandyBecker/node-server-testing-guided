d7d067f7220b8f29ea3466208e4cd4c5e236e538
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-11-14 12:42:58 -0500

    More testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -14,5 +14,27 @@ describe('server', function() {
                 expect(res.status).toBe(200);
             });
         });
+
+        it('should return json formatted response', function() {
+            return request(server)
+            .get('/')
+            .then(res => {
+                expect(res.type).toMatch(/json/i);
+            });
+        });
+
+        it('should return an api property w the value up inside the body', function() {
+            return request(server)
+            .get('/')
+            .then(res => {
+                expect(res.body).toEqual({api:'up'});
+                expect(res.body.api).toBe("up")
+            });
+        });
+
     });
 });
+
+//the endpoint returns the correct http status code based on input
+//the endpoint returns the right data format (json in our case)
+//the endpoint returns the right data in the body based on input
